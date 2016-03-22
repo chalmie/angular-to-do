230392b59b2d171d33b3215bf5fc1f364e871d7b
Blair Peterson and Joseph Karasek <blairchalmers@gmail.com>
2016-03-22 16:01:00 -0700

    edit description now starts with proper priority

--- app/edit-task-details.component.ts
+++ app/edit-task-details.component.ts
@@ -7,6 +7,11 @@ import { Task } from './task.model';
   template: `
   <div class="task-form">
    <h3>Edit Description: </h3>
+   <select class="filter" #newPriority>
+     <option value="low" [selected]="task.priority === 'low'">Low</option>
+     <option value="medium" [selected]="task.priority === 'medium'">Medium</option>
+     <option value="high" [selected]="task.priority === 'high'">High</option>
+   </select>
    <input [(ngModel)]="task.description" class="col-sm-8 input-lg task-form"/>
  </div>
   `
